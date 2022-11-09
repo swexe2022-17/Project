33d6d40c0d9c8a848c240ed07490a7bd054c3d00
Ubuntu <ubuntu@ip-172-31-90-41.ec2.internal>
2022-11-09 07:06:22 +0000

    create controller and model

--- test/controllers/top_controller_test.rb
+++ test/controllers/top_controller_test.rb
@@ -1,19 +1,7 @@
 require 'test_helper'
 
 class TopControllerTest < ActionDispatch::IntegrationTest
-  test "should get user" do
-    get top_user_url
-    assert_response :success
-  end
-
-  test "should get company" do
-    get top_company_url
-    assert_response :success
-  end
-
-  test "should get work" do
-    get top_work_url
-    assert_response :success
-  end
-
+  # test "the truth" do
+  #   assert true
+  # end
 end
