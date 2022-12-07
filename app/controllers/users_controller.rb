6710a4bff82a7ef3be800e63eba363ee798eef21
2011140082 <ubuntu@ip-172-31-90-41.ec2.internal>
2022-12-07 07:57:38 +0000

    inspection

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,14 @@ class UsersController < ApplicationController
   end
   
   def create
-    hashed_password = BCrypt::Password.create(params[:user][:pass])
-    @users = User.new(name: params[:user][:name],pass: hashed_password)
-    @users.save
-    flash[:success] ='新規登録完了'
-    redirect_to root_path
+    hashed_password = BCrypt::Password.create(params[:user][:password])
+    @users = User.new(name: params[:user][:name],password: hashed_password, password_confirmation: params[:user][:password_confirmation])
+    if @users.save
+      flash[:success] ='新規登録完了'
+      redirect_to root_path
+    else
+      flash[:false] = 'パスワードの確認失敗'
+      redirect_to root_path
+    end
   end
 end
