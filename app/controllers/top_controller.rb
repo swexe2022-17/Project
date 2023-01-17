fc3fad8dc8c6bcc9b66d2076ab86b272f72d9c80
2011140082 <ubuntu@ip-172-31-90-41.ec2.internal>
2023-01-17 07:07:40 +0000

    push to render.com

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,34 +3,20 @@ class TopController < ApplicationController
     render 'main'
   end
   def login
-<<<<<<< HEAD
-    if @users = User.find_by(name: params[:name])
-      login_password = BCrypt::Password.new(@users.pass)
+    users = User.find_by(name: params[:name])
+    if users
+      login_password = BCrypt::Password.new(users.pass)
       if login_password = params[:pass]
         session[:login_uid] = params[:name]
-        flash.now[:session] = 'ログイン成功'
-        render 'companies/index'
-=======
-      users = User.find_by(name: params[:name])
-      if users
-        login_password = BCrypt::Password.new(users.pass)
-        if login_password = params[:pass]
-          session[:login_uid] = params[:name]
-          flash.now[:session] = 'ログイン成功'
-          redirect_to companies_path
-        else
-          render 'login'
-        end
->>>>>>> Y2
+        redirect_to companies_path
+      else
+        render 'login'
       end
-    else
-      render 'login'
     end
   end
-  
+ 
   def logout
     session.delete(:login_uid)
-    flash.now[:logout] = 'ログアウト成功'
     render 'main'
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,8 @@ class UsersController < ApplicationController
     hashed_password = BCrypt::Password.create(params[:user][:password])
     @user = User.new(name: params[:user][:name],password: hashed_password, password_confirmation: params[:user][:password_confirmation])
     if @user.save
-      flash[:success] ='新規登録完了'
       redirect_to root_path
     else
-      flash[:false] = 'パスワードの確認失敗'
       redirect_to root_path
     end
   end
